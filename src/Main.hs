fba7e3adad1e87ffdb85b8f68316781f0ef9d9c4
Ole <itstheimp@gmail.com>
2014-11-20 23:14:24 +0100

    Use case for the NodeFilter

--- src/Main.hs
+++ src/Main.hs
@@ -2,31 +2,26 @@
 
 module Main (main) where
 
-import Data.List
+import Data.Maybe
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.Encoding as T
 
 import Text.HTML.TagSoup
-import Network.HTTP.Conduit
-
 import HRel.XML
 
-fetchTags :: String -> IO [Tag T.Text]
-fetchTags = fmap (parseTags . T.decodeUtf8) . simpleHttp
+import Network.HTTP.Conduit
 
-showNode :: Int -> Node T.Text -> String
-showNode n (Node t as cnt cs) =
-	prefix ++ " " ++
-	attrs ++ nl ++
-	T.unpack cnt ++ "\n" ++
-	rest
-	where
-		prefix = concat (replicate n "  ") ++ "+ " ++ T.unpack t
-		nl = "\n" ++ replicate (length prefix + 1) ' '
-		attrs = concat (intersperse nl (map (\(k, v) -> T.unpack k ++ ": " ++ T.unpack v) as))
-		rest = concat (map (showNode (n + 1)) cs)
+fetchContents :: String -> IO T.Text
+fetchContents = fmap T.decodeUtf8 . simpleHttp
 
 main :: IO ()
 main = do
-	tags <- fetchTags "http://www.ddlvalley.rocks/category/tv-shows/hd-720/feed/"
-	putStrLn (showNode 0 (head (toNodeList tags)))
+	txt <- fetchContents "http://www.ddlvalley.rocks/category/tv-shows/hd-720/feed/"
+	case toNodeList (parseTags txt) of
+		[x] -> print . fromJust $ runNodeFilter rssFilter x where
+			rssFilter =
+				node "rss" $ node "channel" $ foreachNode "item" $ do
+					title <- node "title" text
+					urls <- foreachNode "enclosure" (attribute "url")
+					return (title, urls)
+		_ -> putStrLn "Too many nodes"

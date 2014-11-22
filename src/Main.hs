f099a702cbc6bf3bd1ea44889cb56d74f7039736
Ole <itstheimp@gmail.com>
2014-11-22 01:10:47 +0100

    Example implementation for ddlvalley

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,32 @@
 
 module Main (main) where
 
--- import qualified Data.Text.Lazy as T
--- import qualified Data.Text.Lazy.Encoding as T
--- import qualified Data.ByteString.Lazy as B
+import Data.Char
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Encoding as T
 
-import HRel.Markup
+import Control.Monad
+import Control.Monad.Trans.Maybe
+
+import Network.URI
 import Network.HTTP.Conduit
 
+import HRel.Markup
+
 main :: IO ()
-main =
-	simpleHttp "http://www.ddlvalley.rocks/category/tv-shows/hd-720/feed/"
-	>>= print . runNodeFilter rssFilter . parseNode
+main = do
+	txt <- fmap T.decodeUtf8 (simpleHttp "http://www.ddlvalley.rocks/category/tv-shows/hd-720/feed/")
+	case runNodeFilter rssFilter (parseNode txt) of
+		Just pairs -> forM_ pairs displayRelase
+		Nothing    -> putStrLn "Found nothing"
 	where
-		rssFilter =
-			relative $ node "channel" $ foreachNode "item" $ do
-				title <- node "title" text
-				urls <- foreachNode "enclosure" (attribute "url")
-				return (title, urls)
+		rssFilter = relative . node "channel" . foreachNode "item" $ do
+			title <- node "title" text
+			urls <- foreachNode "enclosure" $
+				attribute "url"
+				>>= MaybeT . return . parseURI . T.unpack
+			return (title, urls)
+
+		displayRelase (title, uris) = do
+			print (map (T.dropAround isSpace) (T.split (== '&') title))
+			print uris

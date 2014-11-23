875051c7de033a0c0cada614fb828b88d4662980
Ole <itstheimp@gmail.com>
2014-11-23 17:01:05 +0100

    Auxiliary tools module

---
+++ src/HRel/Tools.hs
@@ -0,0 +1,25 @@
+module HRel.Tools (
+	-- * URI Tools
+	toURI,
+	toHost,
+
+	-- * Text Tools
+	trimText
+) where
+
+import Control.Monad.Trans.Maybe
+import Text.StringLike
+import Network.URI
+
+-- | Parse a string-like type.
+toURI :: (Monad m, StringLike s) => s -> MaybeT m URI
+toURI = MaybeT . return . parseURI . toString
+
+-- | Get the host part of a URI.
+toHost :: (Monad m) => URI -> MaybeT m String
+toHost = MaybeT . return . fmap uriRegName . uriAuthority
+
+-- | Trim space around a text.
+trimText :: T.Text -> T.Text
+trimText = T.dropAround isSpace
+

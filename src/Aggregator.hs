c3e1279a7210abb8f55009b2b37e646f3530647e
Ole <itstheimp@gmail.com>
2014-11-27 17:03:37 +0100

    Complete aggregator

--- src/Aggregator.hs
+++ src/Aggregator.hs
@@ -1,8 +1,13 @@
 module Main (main) where
 
+import Control.Monad
+
 import HRel.Aggregators
+import HRel.Database
 
 main :: IO ()
-main =
-	aggregate
-	>>= print . length
+main = do
+	db <- connectToDatabase
+	rels <- aggregate
+	forM_ rels $ \(names, links) ->
+		insertGroup db names links

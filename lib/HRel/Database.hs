e33a84f8d94f852c30216ecfa4f56b4e1e805830
Ole <itstheimp@gmail.com>
2014-11-27 17:01:15 +0100

    Database module

---
+++ lib/HRel/Database.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HRel.Database (
+	connectToDatabase,
+	insertGroup,
+
+	module Database.MySQL.Simple
+) where
+
+import Data.Word
+import Data.Char
+import qualified Data.Text.Lazy as T
+
+import Control.Monad
+
+import Network.URI (URI)
+
+import Database.MySQL.Simple
+
+-- |
+connectToDatabase :: IO Connection
+connectToDatabase =
+	connect (defaultConnectInfo {connectHost     = "localhost",
+	                             connectDatabase = "hrel"})
+
+-- |
+createGroup :: Connection -> IO Word64
+createGroup db = do
+	execute_ db "INSERT INTO groups () VALUES ()"
+	insertID db
+
+-- |
+insertGroup :: Connection -> [T.Text] -> [URI] -> IO Word64
+insertGroup db names links = do
+	rs <- query db "SELECT nameID, groupID FROM names WHERE searchName IN ?"
+	               (Only (In searchNames))
+	groupID <- createGroup db
+
+	forM_ rs $ \(nameID, oldGroupID) -> do
+		execute db "UPDATE names SET groupID = ? WHERE nameID = ?" (groupID, nameID :: Word64)
+		execute db "UPDATE links SET groupID = ? WHERE groupID = ?" (groupID, oldGroupID :: Word64)
+		execute db "DELETE FROM groups WHERE id = ?" (Only oldGroupID)
+
+	executeMany db "INSERT INTO names (searchName, fullName, groupID) VALUES (?, ?, ?)"
+	               (zipWith (\s f -> (s, f, groupID)) searchNames names)
+	executeMany db "INSERT INTO links (uri, groupID) VALUES (?, ?)"
+	               (map (flip (,) groupID . show) links)
+
+	return groupID
+	where
+		searchNames = map (T.map toLower) names

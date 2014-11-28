3aca9ed24f1ecef7f3e18b2b883c3dafea0091f5
Ole <itstheimp@gmail.com>
2014-11-28 17:15:00 +0100

    Proper documentation for the Database module

--- lib/HRel/Database.hs
+++ lib/HRel/Database.hs
@@ -14,6 +14,7 @@ import Data.List
 import Data.Char
 import qualified Data.Text.Lazy as T
 
+import Control.Applicative
 import Control.Monad
 
 import Network.URI (URI)
@@ -39,32 +40,42 @@ insertGroup :: Connection -> [T.Text] -> [URI] -> IO Word64
 insertGroup db names links = do
 	groupID <- createGroup db
 
+	-- Find groups that might be already in the database with the same release names
 	rs <- query db "SELECT groupID FROM names WHERE searchName IN ? GROUP BY groupID"
 	               (Only (In searchNames))
+
+	-- Connect existing release names and associated links with the new group
 	forM_ rs $ \(Only oldGroupID) -> do
+		-- Move existing release names into the new group
 		execute db "UPDATE names SET groupID = ? WHERE groupID = ?"
 		        (groupID, oldGroupID :: Word64)
+
+		-- Move associated links into the new group
 		execute db "UPDATE links SET groupID = ? WHERE groupID = ?"
 		        (groupID, oldGroupID :: Word64)
+
+		-- Delete the old group
 		execute db "DELETE FROM groups WHERE id = ?"
 		        (Only oldGroupID)
 
+	-- Insert the new release names
 	forM_ (zip searchNames names) $ \(s, n) -> do
 		r <- execute db "INSERT IGNORE INTO names (searchName, fullName, groupID) VALUES (?, ?, ?)"
 		             (s, n, groupID)
+
+		-- Generate search tags, when the insertion was successful (no duplicate entry)
 		when (r > 0) $ do
 			nameID <- insertID db
 			case T.split (== '-') s of
 				[] ->
+					-- No use of '-' means it's a ill-formated release name
 					return ()
 				xs -> do
 					executeMany db "INSERT INTO tags (value, nameID) VALUES (?, ?)"
-					            (map (flip (,) nameID . cleanText)
-					                  . filter (not . T.null)
-					                  . T.split (== '.')
-					                  $ T.concat (init xs))
+					            (tagify nameID xs)
 					return ()
 
+	-- Insert links
 	executeMany db "INSERT IGNORE INTO links (uri, groupID) VALUES (?, ?)"
 	            (map (flip (,) groupID . show) links)
 
@@ -72,17 +83,23 @@ insertGroup db names links = do
 	where
 		searchNames = map (T.map toLower) names
 		cleanText = T.filter (not . isSpace)
+		tagify nameID =
+			map (flip (,) nameID . cleanText)   -- Associate with nameID
+			. filter (not . T.null . cleanText) -- Filter empty segments
+			. T.split (== '.')                  -- Split into tags
+			. T.concat
+			. init                              -- Remove the name of the releasing group
 
 -- | Search for names that match the given tags.
 findNames :: Connection -> [T.Text] -> IO [(Word64, Word64, T.Text)]
 findNames db tags =
 	query db sql (Only (In (map T.toLower tags)))
 	where
+		-- TODO: Sort by groupID instead of nameID
 		sql = "SELECT names.nameID, names.groupID, names.fullName FROM tags, names WHERE tags.value IN ? AND tags.nameID = names.nameID GROUP BY tags.nameID ORDER BY COUNT(tags.value) DESC, names.nameID DESC"
 
--- |
+-- | Find a group by ID
 findGroup :: Connection -> Word64 -> IO ([T.Text], [String])
-findGroup db groupID = do
-	names <- query db "SELECT fullName FROM names WHERE groupID = ?" (Only groupID)
-	links <- query db "SELECT uri FROM links WHERE groupID = ?" (Only groupID)
-	return (map fromOnly names, sort (map fromOnly links))
+findGroup db groupID =
+	(,) <$> (map fromOnly <$> query db "SELECT fullName FROM names WHERE groupID = ?" (Only groupID))
+	    <*> (sort . map fromOnly <$> query db "SELECT uri FROM links WHERE groupID = ?" (Only groupID))

91c1123d92c22956f90a4979c268e4cbf7b39ee0
Ole <itstheimp@gmail.com>
2014-12-02 18:44:06 +0100

    Insert only links that are allowed to be used

--- src/Aggregator.hs
+++ src/Aggregator.hs
@@ -4,10 +4,11 @@ import Control.Monad
 
 import HRel.Aggregators
 import HRel.Database
+import HRel.Tools
 
 main :: IO ()
 main = do
 	db <- connectToDatabase
 	rels <- aggregate
 	forM_ rels $ \(names, links) ->
-		insertGroup db names links
+		insertGroup db names (filter isValidURI links)

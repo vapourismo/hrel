74b679ab57b58c535a352d3aaa3dffc3adbfc8a9
Ole Krüger <ole@vprsm.de>
2015-05-05 21:46:22 +0200

    Add 'kickass.to' search result aggregator

--- src/HRel/Source/KickAss.hs
+++ src/HRel/Source/KickAss.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HRel.Source.KickAss (
 	kickAssHourly,
-	kickAssDaily
+	kickAssDaily,
+	kickAssSearch
 ) where
+
+import Control.Monad.Trans.Maybe
 
 import Data.Maybe
 
@@ -19,6 +24,7 @@ import Network.HTTP.Types
 import Network.URI
 
 import HRel.Source
+import HRel.Markup
 
 -- | Download data dump and parse its contents.
 fetchFromDump :: String -> Manager -> IO [Torrent]
@@ -45,6 +51,39 @@ fetchFromDump url mgr = do
 
 		pickTorrents = catMaybes . map toTorrent . T.lines
 
+-- |
+fetchFromRSS :: String -> Manager -> IO [Torrent]
+fetchFromRSS url mgr = do
+	req <- parseUrl url
+	withResponse req mgr $ \ res ->
+		case responseStatus res of
+			Status 200 _ -> do
+				fmap (runFilter . fromMarkup' . decode) (brConsume (responseBody res))
+					>>= print
+				return []
+
+			Status _   _ ->
+				return []
+		where
+			decode = T.decodeUtf8 . B.concat
+
+			toURI = MaybeT . return . parseURI . T.unpack
+
+			rssFilter =
+				relativeTag "rss" $ forTag "channel" $
+					foreachTag "item" $ do
+						title <- forTag "title" text
+						size <- forTag "torrent:contentLength" text
+						magnetURI <- forTag "torrent:magnetURI" text
+						             >>= toURI . T.strip
+						torrentURI <- forTag "enclosure" (attr "url")
+						              >>= toURI . fst . T.break (== '?') . T.strip
+						return (Torrent (T.strip title)
+						                [magnetURI, torrentURI]
+						                (Just (read (T.unpack (T.strip size)))))
+
+			runFilter = runNodeFilter rssFilter
+
 -- | Hourly dump from 'kickass.to'
 kickAssHourly :: Aggregator
 kickAssHourly =	Aggregator (fetchFromDump "https://kickass.to/hourlydump.txt.gz")
@@ -52,3 +91,10 @@ kickAssHourly =	Aggregator (fetchFromDump "https://kickass.to/hourlydump.txt.gz"
 -- | Daily dump from 'kickass.to'
 kickAssDaily :: Aggregator
 kickAssDaily = Aggregator (fetchFromDump "https://kickass.to/dailydump.txt.gz")
+
+-- | Search on 'kickass.to'
+kickAssSearch :: String -> Aggregator
+kickAssSearch term =
+	Aggregator (fetchFromRSS ("https://kickass.to/usearch/"
+	                          ++ escapeURIString isUnescapedInURI term
+	                          ++ "/?rss=1"))

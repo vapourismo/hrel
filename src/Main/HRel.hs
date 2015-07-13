7ee8c9a719501087478fdc81db7e97bc484869ff
Ole <ole@vprsm.de>
2015-07-14 00:32:47 +0200

    Rename main module file

--- src/Main/HRel.hs
+++ src/Main/HRel.hs
@@ -5,15 +5,14 @@ import qualified Data.Conduit.List as C
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 
-import HRel.Source.XRel
+import HRel.Source.Feeds
 import HRel.Conduit
 
 main :: IO ()
-main = do
+main =
 	withManager tlsManagerSettings $ \ mgr -> do
-		contents <- runHRelConduit mgr (xrelFavourites xrelURL =$= C.consume)
-		print contents
-		print (length contents)
+		contents <- runHRelConduit mgr (fromRSSTitles xrelURL =$= C.consume)
+		mapM_ print contents
 
 	where
 		xrelURL = "http://www.xrel.to/releases-usrss.html?u=20470&s=ee663473a8da8a161902c908326ebe1c&favs=1"

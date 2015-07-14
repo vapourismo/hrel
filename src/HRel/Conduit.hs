be3870b3260483f708700eaf46b42fdae50ef213
Ole <ole@vprsm.de>
2015-07-14 21:10:37 +0200

    Add conduit worker threads

--- src/HRel/Conduit.hs
+++ src/HRel/Conduit.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module HRel.Conduit (
 	-- * Types
 	HRel (..),
@@ -10,6 +12,8 @@ module HRel.Conduit (
 	HRelSink,
 	runHRelConduit,
 
+	forkWorkerConduit,
+
 	-- * Utilities
 	request,
 	fetch,
@@ -20,15 +24,17 @@ module HRel.Conduit (
 	module Data.Conduit
 ) where
 
+import           Control.Concurrent hiding (yield)
+import           Control.Exception
 import           Control.Monad.Reader
 
 import qualified Codec.Compression.GZip as Z
 
-import qualified Data.ByteString        as B
-import qualified Data.ByteString.Lazy   as BL
+import           Data.Void
 import           Data.Conduit
 import qualified Data.Conduit.List      as C
-import           Data.Void
+import qualified Data.ByteString        as B
+import qualified Data.ByteString.Lazy   as BL
 
 import           Text.StringLike
 
@@ -44,7 +50,7 @@ type HRelT = ReaderT HRel IO
 type HRelConduitM i o r = ConduitM i o HRelT r
 
 -- | Source
-type HRelSource o = HRelConduitM () o ()
+type HRelSource o = HRelConduit () o
 
 -- | Conduit
 type HRelConduit i o = HRelConduitM i o ()
@@ -67,13 +73,13 @@ fetchLazy :: HRelConduit Request BL.ByteString
 fetchLazy =
 	C.mapMaybeM $ \ req -> do
 		HRel mgr <- ask
-		res <- liftIO (httpLbs req mgr)
-
-		pure $
-			if responseStatus res == status200 then
-				Just (responseBody res)
-			else
-				Nothing
+		liftIO $ handle (\ (SomeException _) -> pure Nothing) $ do
+			res <- httpLbs req mgr
+			pure $
+				if responseStatus res == status200 then
+					Just (responseBody res)
+				else
+					Nothing
 
 -- | Perform a request and retrieve the result body.
 fetch :: HRelConduit Request B.ByteString
@@ -89,3 +95,38 @@ fetchGZipped =
 markup :: (StringLike t) => NodeFilterT t HRelT a -> HRelConduit t a
 markup nf =
 	C.mapMaybeM (runNodeFilterT nf . fromMarkup')
+
+-- | Create a source for which takes its outputs from a channel.
+sourceChan :: (MonadIO m) => Int -> Chan (Maybe a) -> Source m a
+sourceChan num chan =
+	when (num > 0) $ do
+		msg <- liftIO (readChan chan)
+		case msg of
+			Nothing ->
+				sourceChan (num - 1) chan
+
+			Just x -> do
+				yield x
+				sourceChan num chan
+
+-- | Create a sink for directs its inputs to a channel
+sinkChan :: (MonadIO m) => Int -> Chan (Maybe a) -> Sink a m ()
+sinkChan num chan = do
+	msg <- await
+	case msg of
+		Nothing ->
+			liftIO (replicateM_ num (writeChan chan Nothing))
+
+		value -> do
+			liftIO (writeChan chan value)
+			sinkChan num chan
+
+-- | Fork a worker thread which
+forkWorkerConduit :: Int -> Manager -> HRelConduit i o -> IO (HRelSink i (), HRelSource o)
+forkWorkerConduit num mgr conduit = do
+	(input, output) <- (,) <$> newChan <*> newChan
+
+	replicateM_ num $ forkIO $
+		runHRelConduit mgr (sourceChan 1 input =$= conduit =$= sinkChan 1 output)
+
+	pure (sinkChan num input, sourceChan num output)

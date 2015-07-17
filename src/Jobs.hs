aa2f56fa2f6e8ab1db21d9a3011fb056461a5262
Ole <ole@vprsm.de>
2015-07-17 22:34:20 +0200

    Seperate processing module

--- src/Jobs.hs
+++ src/Jobs.hs
@@ -1,10 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Monad
-import           Control.Monad.Trans
-import           Control.Monad.Catch
-
-import           Data.Word
 import           Data.Conduit
 import qualified Data.Conduit.List as C
 
@@ -12,39 +7,8 @@ import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 
 import           HRel.Source.Feeds
-import           HRel.Source.KickAssTorrents
-import           HRel.Release
-import           HRel.Torrent
+import           HRel.Processing
 import           HRel.Database
-
-trackReleases :: (MonadIO m) => Database -> Conduit (Word64, Release) m (Word64, Release)
-trackReleases db =
-	C.mapMaybeM $ \ (fid, rel) ->
-		runAction db $ do
-			mbReleaseID <- insert "INSERT INTO releases (feed, name, track) values (?, ?, true) ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id)" (fid, fromRelease rel)
-			case mbReleaseID of
-				Just releaseID -> do
-					mbTracks <- query "SELECT track FROM releases WHERE id = ?" (Only releaseID)
-
-					pure $ case mbTracks of
-						[Only True] -> Just (releaseID, rel)
-						_           -> Nothing
-
-				Nothing ->
-					pure Nothing
-
-findTorrents :: (MonadIO m, MonadThrow m) => Manager -> Conduit (Word64, Release) m (Word64, Torrent)
-findTorrents mgr =
-	C.concatMapM $ \ (rid, rel) -> do
-		torrents <- yield rel $$ kickAssSearch mgr =$= C.consume
-		pure (map ((,) rid) torrents)
-
-trackTorrents :: (MonadIO m) => Database -> Sink (Word64, Torrent) m ()
-trackTorrents db =
-	C.mapM_ $ \ (rid, torrent) ->
-		runAction db $
-			forM_ (torrentSource torrent) $ \ src ->
-				execute "INSERT INTO torrents (rel, url, size) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id)" (rid, show src, torrentContentSize torrent)
 
 main :: IO ()
 main = withDatabase $ \ db -> withManager tlsManagerSettings $ \ mgr -> do

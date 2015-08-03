949bfd4e8914991d7992da40ecd04b03834b90a3
Ole <ole@vprsm.de>
2015-08-03 23:52:41 +0200

    Clear KickAssTorrents module from conduit attachments

--- lib/HRel/Source/KickAssTorrents.hs
+++ lib/HRel/Source/KickAssTorrents.hs
@@ -1,64 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module HRel.Source.KickAssTorrents (
-	kickAssSearch
+	kickAssSearchFilter,
+	parseKickAssSearch,
+	searchKickAss
 ) where
 
 import           Control.Monad
-import           Control.Monad.Trans
-import           Control.Monad.Catch
 
 import           Data.Char
 import           Data.Maybe
-import           Data.Conduit
-import qualified Data.Conduit.List  as C
-import qualified Data.Text          as T
-import qualified Data.Text.Encoding as T
+import qualified Data.Text             as T
+import qualified Data.Text.Encoding    as T
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Lazy  as BL
 
 import           Network.URI
 import           Network.HTTP.Client
+import           Network.HTTP.Types.Status
 
-import           HRel.Conduit
 import           HRel.Markup
 import           HRel.Data.Release
 import           HRel.Data.Torrent
 
--- | Search for torrents which match the given release name.
-kickAssSearch :: (MonadIO m, MonadThrow m) => Manager -> Conduit ReleaseName m TorrentInfo
-kickAssSearch mgr =
-	await >>= maybe (pure ()) (\ rel -> performSearch rel >> kickAssSearch mgr)
+-- | Filter for KickAss Torrents RSS search result.
+kickAssSearchFilter :: NodeFilter B.ByteString [TorrentInfo]
+kickAssSearchFilter =
+	fmap concat $ relativeTag "rss" $ forTag "channel" $ foreachTag "item" $ do
+		title   <- forTag "title"                 (T.strip . T.decodeUtf8 <$> text)
+		size    <- forTag "torrent:contentLength" (T.strip . T.decodeUtf8 <$> text)
+		magnet  <- forTag "torrent:magnetURI"     (T.strip . T.decodeUtf8 <$> text)
+		torrent <- forTag "enclosure"             (T.strip . T.decodeUtf8 <$> attr "url")
+
+		let uris = catMaybes [parseURI (T.unpack magnet), parseURI (cleanTorrentURL torrent)]
+		guard (length uris > 0)
+
+		let sizeNum =
+			if T.all isDigit size then
+				Just (read (T.unpack size))
+			else
+				Nothing
+
+		pure (map (\ uri -> TorrentInfo (normalizeReleaseName title) uri sizeNum) uris)
 	where
-		performSearch rel =
-			request ("https://kat.cr/usearch/"
-			         ++ escapeURIString isUnescapedInURI (T.unpack (getReleaseName rel))
-			         ++ "/?rss=1")
-				=$= fetch mgr
-				=$= C.map T.decodeUtf8
-				=$= markup (xmlFilter rel)
-				=$= C.concat
+		cleanTorrentURL =  T.unpack . fst . T.break (== '?')
 
-		xmlFilter cmpRelease =
-			fmap concat $ relativeTag "rss" $ forTag "channel" $ foreachTag "item" $ do
-				title   <- forTag "title"                 (T.strip <$> text)
-				size    <- forTag "torrent:contentLength" (T.strip <$> text)
-				magnet  <- forTag "torrent:magnetURI"     (T.strip <$> text)
-				torrent <- forTag "enclosure"             (T.strip <$> attr "url")
+-- | Parse the result to a KickAss Torrents RSS search.
+parseKickAssSearch :: B.ByteString -> Maybe [TorrentInfo]
+parseKickAssSearch contents =
+	runNodeFilter kickAssSearchFilter (fromMarkup' contents)
 
-				let release = normalizeReleaseName (T.copy title)
-				guard (release == cmpRelease)
-
-				let uris = catMaybes [makeURI magnet,
-				                      makeURI (cleanTorrentURI torrent)]
-				guard (length uris > 0)
-
-				let sizeNum =
-					if T.all isDigit size then
-						Just (read (T.unpack size))
-					else
-						Nothing
-
-				pure (map (\ uri -> TorrentInfo release uri sizeNum) uris)
-
-		cleanTorrentURI = fst . T.break (== '?')
-
-		makeURI = parseURI . T.unpack
+-- | Use KickAss Torrents RSS search to file torrents matching a given release name.
+searchKickAss :: Manager -> ReleaseName -> IO (Maybe [TorrentInfo])
+searchKickAss mgr rel = do
+	req <- parseUrl requestURL
+	res <- httpLbs req mgr
+	pure $ case responseStatus res of
+		Status 200 _ -> parseKickAssSearch (BL.toStrict (responseBody res))
+		_            -> Nothing
+	where
+		requestURL =
+			"https://kat.cr/usearch/"
+			++ escapeURIString isUnescapedInURI (T.unpack (getReleaseName rel))
+			++ "/?rss=1"

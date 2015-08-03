4dbf0e47b965994bf67cb1ee4e0119d30f8fe4e3
Ole <ole@vprsm.de>
2015-08-03 23:27:18 +0200

    Purge Feeds module

--- lib/HRel/Source/Feeds.hs
+++ lib/HRel/Source/Feeds.hs
@@ -1,30 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module HRel.Source.Feeds (
-	fromAtomTitles
+	-- * Atom Feeds
+	parseAtomFeed,
+	parseAtomFeedRequest,
+	parseAtomFeedURL,
 ) where
 
-import           Control.Monad.Trans
-import           Control.Monad.Catch
-
-import           Data.List
-import qualified Data.Conduit.List   as C
-import qualified Data.Text.Encoding  as T
+import qualified Data.ByteString      as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text.Encoding   as T
 
 import           Network.HTTP.Client
+import           Network.HTTP.Types.Status
 
-import           HRel.Conduit
 import           HRel.Markup
 import           HRel.Data.Release
 
--- | Extract releases from Atom entry titles.
-fromAtomTitles :: (MonadIO m, MonadThrow m) => Manager -> String -> Source m ReleaseName
-fromAtomTitles mgr url =
-	request url =$= fetch mgr =$= C.map T.decodeUtf8
-	            =$= markup rootFilter =$= C.map nub =$= C.concat
+-- | Parse the contents of an Atom feed in order to extract the release names from entry titles.
+parseAtomFeed :: B.ByteString -> Maybe [ReleaseName]
+parseAtomFeed contents =
+	runNodeFilter rootFilter (fromMarkup' contents)
 	where
 		titleFilter =
-			normalizeReleaseName <$> text
+			normalizeReleaseName . T.decodeUtf8 <$> text
 
 		rootFilter =
 			reverse <$> relativeTag "feed" (foreachTag "entry" (forTag "title" titleFilter))
+
+-- | Same as "parseAtomFeed" but does the download also.
+parseAtomFeedRequest :: Manager -> Request -> IO (Maybe [ReleaseName])
+parseAtomFeedRequest mgr req = do
+	res <- httpLbs req mgr
+	case responseStatus res of
+		Status 200 _ -> pure (parseAtomFeed (BL.toStrict (responseBody res)))
+		_            -> pure Nothing
+
+-- | Same as "parseAtomFeedRequest" but generates the request for you.
+parseAtomFeedURL :: Manager -> String -> IO (Maybe [ReleaseName])
+parseAtomFeedURL mgr url =
+	case parseUrl url of
+		Just req -> parseAtomFeedRequest mgr req
+		Nothing  -> pure Nothing

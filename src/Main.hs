9974ef1af7c0a9fa0cf518ea2a83e980126fd6ff
Ole <ole@vprsm.de>
2015-08-05 15:19:42 +0200

    Fix typo

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,6 @@ import HRel.Processing
 main :: IO ()
 main = withManifest $ \ mf -> do
 	spawnWorkers mf
-	spawnJobTimers mf
+	spawnJobTimer mf
 	processAllFeeds mf
 	forever (threadDelay 1000 >> yield)

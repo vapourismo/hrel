38e94d604ae46885c26cd9d97cdac4cf80ff5794
Ole <ole@vprsm.de>
2015-11-14 00:52:34 +0100

    Bundle all hrel-sources modules in HRel.Sources

---
+++ lib/HRel/Sources.hs
@@ -0,0 +1,8 @@
+module HRel.Sources (
+	module Sources
+) where
+
+import HRel.Sources.ReleaseName     as Sources
+import HRel.Sources.ReleaseFeeds    as Sources
+import HRel.Sources.TorrentInfo     as Sources
+import HRel.Sources.KickAssTorrents as Sources

9f971bd7cbcfc56fd1a49b471fe372fce887306a
Ole <ole@vprsm.de>
2015-11-14 18:05:26 +0100

    Clean directory

--- lib/HRel/Sources.hs
+++
@@ -1,8 +0,0 @@
-module HRel.Sources (
-	module Sources
-) where
-
-import HRel.Sources.ReleaseName     as Sources
-import HRel.Sources.ReleaseFeeds    as Sources
-import HRel.Sources.TorrentInfo     as Sources
-import HRel.Sources.KickAssTorrents as Sources

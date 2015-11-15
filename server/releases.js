b5928b4b6c3d5126e01a685d094a9a85b3235115
Ole <ole@vprsm.de>
2015-11-15 15:17:09 +0100

    Improve upsertion of releases

--- server/releases.js
+++ server/releases.js
@@ -5,14 +5,8 @@ const db = require("./database");
 const table = new db.Table("releases", "id", ["name"]);
 
 const insert = function* (name) {
-	try {
-		return yield table.insert({name});
-	} catch (error) {
-		if (error.code == 23505 && error.constraint == "releases_name_key")
-			return (yield table.find({name})).pop();
-		else
-			throw error;
-	}
+	yield db.query("INSERT INTO releases (name) SELECT $1 :: varchar WHERE NOT EXISTS (SELECT * FROM releases WHERE name = $1 :: varchar)", [name]);
+	return (yield table.find({name})).pop();
 }.async;
 
 module.exports = {

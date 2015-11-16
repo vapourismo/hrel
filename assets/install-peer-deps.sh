3ba84358f1b2635dd9ea16cf6d52d0c609e821db
Ole <ole@vprsm.de>
2015-11-17 00:56:14 +0100

    Add 'page' as peer dependency

--- assets/install-peer-deps.sh
+++ assets/install-peer-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npm install react react-dom superagent babel-core babel-loader babel-plugin-transform-runtime babel-preset-es2015 babel-preset-react css-loader node-sass sass-loader style-loader webpack
+npm install page react react-dom superagent babel-core babel-loader babel-plugin-transform-runtime babel-preset-es2015 babel-preset-react css-loader node-sass sass-loader style-loader webpack

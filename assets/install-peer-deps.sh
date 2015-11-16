df21212a403ce2078aeb9ea57c3685879e244c6c
Ole <ole@vprsm.de>
2015-11-17 00:50:37 +0100

    Seperate peer dependencies

---
+++ assets/install-peer-deps.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npm install react react-dom superagent xml2js babel-core babel-loader babel-plugin-transform-runtime babel-preset-es2015 babel-preset-react css-loader node-sass sass-loader style-loader webpack

3c2c0f989992386483e524410823a4ee4453d216
Ole <ole@vprsm.de>
2016-04-26 17:25:35 +0200

    Let http.download follow up to 10 redirections

--- server/http.js
+++ server/http.js
@@ -6,68 +6,121 @@ const https = require("https");
 const http  = require("http");
 const zlib  = require("zlib");
 const url   = require("url");
+const util  = require("./utilities");
+
+/**
+ * Generate a HTTP request and process its result.
+ * @param {Object or String} options Requested URL or request options
+ * @param {Function} accept  Invoked when the response object is valid and has status code 200
+ * @param {Function} reject  Invoked upon error
+ */
+function makeRequest(options, accept, reject, redirects) {
+	redirects = redirects || 0;
+
+	if (!(options instanceof Object))
+		options = url.parse(options);
+
+	let schema = options.protocol == "https:" ? https : http;
+
+	const req = schema.request(options, function (response) {
+		processResponse(response, accept, reject, redirects);
+	});
+
+	req.on("error", reject);
+	req.end();
+}
+
+/**
+ * Process a HTTP response. This function will follow up to 10 redirections.
+ * @param {Response} response  Response object
+ * @param {Function} accept    Invoked when the response object is valid and has status code 200
+ * @param {Function} reject    Invoked upon error
+ * @param {Number}   redirects Number of redirects which have been followed so far
+ */
+function processResponse(response, accept, reject, redirects) {
+	if (redirects >= 10)
+		return reject(new Error("Maximum number of redirections exceeded"));
+
+	switch (response.statusCode) {
+		case 200:
+			accept(response);
+			break;
+
+		case 301:
+			if ("location" in response.headers)
+				makeRequest(response.headers["location"], accept, reject, redirects + 1);
+			else
+				reject(new Error("Response is missing a location header"));
+
+			break;
+
+		default:
+			reject(new Error("Invalid response status code (" + response.statusMessage + ")"));
+			break;
+	}
+}
+
+/**
+ * Wrap a HTTP request in a Promize.
+ * @param {Object or String} options Requested URL or request options
+ * @returns {Promise<Response>}
+ */
+function request(options) {
+	return new Promise(function (accept, reject) {
+		makeRequest(options, accept, reject, 0);
+	});
+}
 
 /**
  * Download via HTTP. Also decode the contents.
  * @param {Object or String} options Requested URL or request options
  * @returns {Promise<Buffer>}
  */
-function download(options, maxContentLength) {
-	if (!(options instanceof Object))
-		options = url.parse(options);
+const download = function* (options, maxContentLength) {
+	const res = yield request(options);
 
-	let schema = options.protocol == "https:" ? https : http;
+	if (maxContentLength && "content-length" in res.headers && res.headers["content-length"] > maxContentLength)
+		return reject(new Error("Content too long"));
 
-	return new Promise(function (accept, reject) {
-		const req = schema.request(options, function (res) {
-			if (res.statusCode != 200)
-				return reject(new Error("Invalid response status code (" + res.statusMessage + ")"));
+	return yield new Promise(function (accept, reject) {
+		let received = 0;
+		const chunks = [];
 
-			let received = 0;
-			const chunks = [];
-
-			if (maxContentLength && "content-length" in res.headers && res.headers["content-length"] > maxContentLength)
+		res.on("data", function (chunk) {
+			if (maxContentLength && received + chunk.length > maxContentLength) {
+				res.removeAllListeners();
 				return reject(new Error("Content too long"));
+			}
 
-			res.on("data", function (chunk) {
-				if (maxContentLength && received + chunk.length > maxContentLength) {
-					res.removeAllListeners();
-					return reject(new Error("Content too long"));
-				}
-
-				chunks.push(chunk);
-				received += chunk.length;
-			});
-
-			res.on("end", function () {
-				const data = Buffer.concat(chunks);
-
-				switch (res.headers["content-encoding"]) {
-					case "gzip":
-						zlib.gunzip(data, function (error, result) {
-							if (error) reject(error);
-							else       accept(result);
-						});
-						break;
-
-					case "deflate":
-						zlib.inflate(data, function (error, result) {
-							if (error) reject(error);
-							else       accept(result);
-						});
-						break;
-
-					default:
-						accept(data);
-						break;
-				}
-			});
+			chunks.push(chunk);
+			received += chunk.length;
 		});
 
-		req.on("error", reject);
-		req.end();
+		res.on("end", function () {
+			const data = Buffer.concat(chunks);
+
+			switch (res.headers["content-encoding"]) {
+				case "gzip":
+					zlib.gunzip(data, function (error, result) {
+						if (error) reject(error);
+						else       accept(result);
+					});
+					break;
+
+				case "deflate":
+					zlib.inflate(data, function (error, result) {
+						if (error) reject(error);
+						else       accept(result);
+					});
+					break;
+
+				default:
+					accept(data);
+					break;
+			}
+		});
 	});
-}
+}.async;
 
 function stream(options) {
 	if (!(options instanceof Object))

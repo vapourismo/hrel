81f03c6ddcfe35d59ce630f0ad24b6f68357cb5a
Ole <ole@vprsm.de>
2016-04-28 00:31:16 +0200

    Simplify configuration module

--- server/config.js
+++ server/config.js
@@ -1,43 +1,23 @@
 /* Copyright (C) 2015-2016, Ole Krüger <ole@vprsm.de> */
 
-const fs = require("fs");
-
-const defaultConfig = {
+module.exports = {
+	// Database settings (passed to pg.Client)
 	database: {
 		host: "localhost",
 		user: "hrel",
 		database: "hrel"
 	},
+
+	// Web server settings (passed to http.Server#listen)
 	server: {
 		host: "127.0.0.1",
 		port: 3102,
 		exclusive: false
 	},
+
+	// Output coloured logs?
 	colorLogging: true,
+
+	// Use 'pg-native' instead of the 'pg' package
 	usePGNative: true
 };
-
-function validateObject(schema, instance) {
-	if (typeof(schema) != typeof(instance))
-		return schema;
-
-	if (schema instanceof Array) {
-		return instance;
-	} else if (schema instanceof Object) {
-		const object = {};
-
-		for (var key in schema) {
-			object[key] = validateObject(schema[key], instance[key]);
-		}
-
-		return object;
-	} else {
-		return instance;
-	}
-}
-
-try {
-	module.exports = validateObject(defaultConfig, JSON.parse(fs.readFileSync("config.json")));
-} catch (e) {
-	module.exports = defaultConfig;
-}

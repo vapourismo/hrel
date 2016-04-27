dfa9345129b9a44d1d0fb7a314a644880e423e68
Ole <ole@vprsm.de>
2016-04-27 15:02:01 +0200

    Migrate build tools to npm

--- assets/build-client-side.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Generate client-side files
-webpack -p
-node-sass client/source/index.scss client/static/index.css
--- assets/development-server.js
+++ assets/development-server.js
@@ -1,15 +1,16 @@
 "use strict";
 
-const fs    = require("fs");
-const path  = require("path");
-const child = require("child_process");
+const fs      = require("fs");
+const path    = require("path");
+const child   = require("child_process");
+const webpack = require("webpack");
+const util    = require("../server/utilities");
 
 const serverModule = path.join(path.dirname(module.filename), "..", "server", "server.js");
 
 function forkServerModule() {
-	return child.fork(serverModule, [], {
-		execArgv: ["--harmony"]
-	});
+	util.inform("dev-server", "Spawning server module");
+	return child.fork(serverModule);
 }
 
 let serverInstance = forkServerModule();
@@ -18,9 +19,35 @@ fs.watch("server", {persistent: true, recursive: true}, function (event, path) {
 	if (event != "change")
 		return;
 
-	console.log("Terminating server module ...");
+	util.inform("dev-server", "Terminating server module");
 	serverInstance.kill();
 
-	console.log("Respawning module ...");
 	serverInstance = forkServerModule();
+});
+
+webpack(require("../webpack.config.js")).watch({poll: false}, function (error, stats) {
+	if (error)
+		return util.logError(error);
+
+	util.inform("dev-server", "Webpack compiled resources");
+});
+
+function compileSCSS() {
+	const sassProc = child.exec("sass client/source/index.scss:client/static/index.css");
+
+	sassProc.on("exit", function (code, signal) {
+		if (code == 0)
+			util.inform("dev-server", "Sass compiled resources");
+		else
+			util.error("dev-server", "Sass exited with status", code);
+	});
+}
+
+compileSCSS();
+
+fs.watch("client/source/index.scss", {}, function (event, path) {
+	if (event != "change")
+		return;
+
+	compileSCSS();
 });

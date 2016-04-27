4a2d55ced9c08b24cbf867875d0444740e785a62
Ole <ole@vprsm.de>
2016-04-27 15:06:40 +0200

    Migrate 'reset-database' tool to npm

---
+++ assets/reset-database.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const db   = require("../server/database");
+const util = require("../server/utilities");
+
+const run = function* () {
+	yield db.query("DELETE FROM torrents");
+	yield db.query("DELETE FROM feed_contents");
+	yield db.query("DELETE FROM releases");
+	yield db.query("DELETE FROM feeds");
+
+	db.end();
+}.async;
+
+run().catch(error => {
+	util.logError(error);
+	process.exit(1);
+});
--- assets/reset-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-(
-	echo "DELETE FROM torrents;"
-	echo "DELETE FROM feed_contents;"
-	echo "DELETE FROM releases;"
-	echo "DELETE FROM feeds;"
-) | psql hrel hrel

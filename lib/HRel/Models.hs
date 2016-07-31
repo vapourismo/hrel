75723cbebb2a15acd699eb650e60b1662089a513
Ole <ole@vprsm.de>
2016-07-31 18:52:57 +0200

    Do not include profiling options in cabal-file

--- lib/HRel/Models.hs
+++ lib/HRel/Models.hs
@@ -1,3 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module HRel.Models where
+module HRel.Models (
+	Release (..)
+) where
+
+import qualified Data.Text as T
+import           Database.PostgreSQL.Store
+
+data Release = Release {
+	releaseName :: T.Text
+} deriving (Show, Eq, Ord)
+
+mkTable ''Release [Unique ['releaseName]]
--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,7 @@
 
 module Main where
 
-import Control.Monad.Trans.Maybe
-
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
-
-import HRel.Feeds
+import HRel.Models
 
 main :: IO ()
-main = do
-	mgr <- newManager tlsManagerSettings
-
-	res <- runMaybeT (downloadFeed mgr "http://www.movie-blog.org/feed/")
-	print res
+main = pure ()

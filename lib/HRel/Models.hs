39b54caf33720e0851f66ef09bb4ec8a5e0035bb
Ole <ole@vprsm.de>
2016-12-08 22:41:59 +0100

    Strip 'pg-store' imports

--- lib/HRel/Models.hs
+++ lib/HRel/Models.hs
@@ -1,24 +1,17 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module HRel.Models (
 	Release (..),
 	Torrent (..)
 ) where
 
 import qualified Data.Text as T
-import           Database.PostgreSQL.Store
 
 -- |
 data Release = Release {
 	releaseName :: T.Text
 } deriving (Show, Eq, Ord)
 
-mkTable ''Release [Unique ['releaseName]]
-
 -- |
 data Torrent = Torrent {
 	torrentTitle :: T.Text,
 	torrentURI   :: T.Text
 } deriving (Show, Eq, Ord)
-
-mkTable ''Torrent [Unique ['torrentTitle, 'torrentURI]]

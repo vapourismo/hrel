ebf99597538049964503ae15b782627658a17f17
Ole <ole@vprsm.de>
2016-12-23 15:20:02 +0100

    Implement client interface

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,77 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import SuperAgent from "superagent";
 
 // Root
 class Root extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			results: []
+		};
+
+		this.submitSeach = this.submitSeach.bind(this);
+	}
+
+	performSearch(query) {
+		SuperAgent.post("/api/search")
+			.set("Accept", "application/json")
+			.set("Content-Type", "text/plain")
+			.send(query)
+			.end((err, res) => {
+				if (!err && res.statusCode == 200 && res.body instanceof Array) {
+					this.setState({results: res.body});
+				} else {
+					console.error(err, res);
+				}
+			});
+	}
+
+	submitSeach(ev) {
+		if (ev)
+			ev.preventDefault();
+
+		if (this.inputElem) {
+			const queryString = this.inputElem.value.trim();
+
+			if (queryString == "")
+				this.inputElem.select();
+			else
+				this.performSearch(queryString);
+		}
+
+		return false;
+	}
+
+	renderResult(result, idx) {
+		const downloaderLink = "https://www.premiumize.me/downloader?magnet=" + encodeURIComponent(result.uri);
+
+		return (
+			<div className="result" key={idx}>
+				<div className="cell title">{result.title}</div>
+				<a className="cell link" target="blank" href={result.uri}>link</a>
+				<a className="cell add" target="blank" href={downloaderLink}>add</a>
+			</div>
+		);
+	}
+
 	render() {
-		return null;
+		return (
+			<div className="content">
+				<form className="search-form" onSubmit={this.submitSeach}>
+					<input className="input" type="text" ref={elem => this.inputElem = elem} />
+					<div className="submit-button" onClick={this.submitSeach}>
+						Search
+					</div>
+				</form>
+				<div className="results">
+					{this.state.results.length > 0 ? this.state.results.map(this.renderResult)
+						                           : <div className="empty">Empty result set</div>}
+				</div>
+			</div>
+		);
 	}
 }
 

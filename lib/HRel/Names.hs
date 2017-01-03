b48217e1c45c63b8f5492737af86df81f168e4de
Ole <ole@vprsm.de>
2017-01-03 17:44:30 +0100

    Filter duplicates from name tags

--- lib/HRel/Names.hs
+++ lib/HRel/Names.hs
@@ -5,9 +5,10 @@ module HRel.Names (
 ) where
 
 import           Data.Char
+import           Data.List
 import qualified Data.Text as T
 
 -- | Retrieve tags within the name without sorting out the qualifiers.
 parseTags :: T.Text -> [T.Text]
 parseTags name =
-	filter (not . T.null) (T.split (not . isAlphaNum) (T.toLower name))
+	nub (filter (not . T.null) (T.split (not . isAlphaNum) (T.toLower name)))

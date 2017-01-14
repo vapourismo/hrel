71a7fa5be77d2f06b54ed415fda016f6430217d9
Ole <ole@vprsm.de>
2017-01-14 20:06:58 +0100

    Implement example scanner in 'Test'

--- src/Test.hs
+++ src/Test.hs
@@ -3,45 +3,23 @@
 import           Control.Monad.Trans
 import           Control.Monad.Trans.Resource
 
-import qualified Data.Text as T
-
 import           Data.Conduit
 import qualified Data.Conduit.List as C
-import           Data.Conduit.Text
-import           Data.Conduit.Attoparsec
 
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 
-import qualified HRel.XML as X
-import           HRel.Markup2
-import           HRel.NodeFilter2
-import           HRel.Network2
+import           HRel.Sources2
+import           HRel.Torrents
 
-type Torrent = (T.Text, T.Text)
-
-rarbgSource :: NodeFilter [Torrent]
-rarbgSource =
-	"channel" $/ "item" $//
-		buildTorrent <$> ("title" $/ text)
-		             <*> ("link" $/ text)
-	where
-		buildTorrent title uri =
-			(T.strip title, T.strip uri)
-
-source :: (MonadResource m) => Manager -> String -> NodeFilter [Torrent] -> Conduit i m [Torrent]
-source mgr url nf =
-	httpRequest mgr url
-		=$= decode utf8
-		=$= conduitParser X.content
-		=$= C.map snd
-		=$= buildNodes
-		=$= filterNodes nf
+sources :: (MonadResource m) => Manager -> Producer m [Torrent]
+sources mgr =
+	mapM_ (torrentSource mgr)
+	      [RARBG "https://rarbg.to/rssdd_magnet.php?category=41",
+	       RARBG "https://rarbg.to/rssdd_magnet.php?category=48;44;45;42"]
 
 main :: IO ()
 main = do
 	mgr <- newManager tlsManagerSettings
 	runResourceT $ runConduit $
-		(source mgr "https://rarbg.to/rssdd_magnet.php?category=41" rarbgSource
-		 >> source mgr "https://rarbg.to/rssdd_magnet.php?category=48;44;45;42" rarbgSource)
-			=$= C.mapM_ (liftIO . mapM_ print)
+		sources mgr =$= C.mapM_ (liftIO . mapM_ print)

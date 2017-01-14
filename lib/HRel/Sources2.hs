7aba01f11d77e58f6662b7f49342e8344f9af9a8
Ole <ole@vprsm.de>
2017-01-14 20:03:39 +0100

    Add 'Sources2' module

---
+++ lib/HRel/Sources2.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings, RankNTypes #-}
+
+module HRel.Sources2 (
+	pirateBaySource,
+	rarbgSource,
+
+	TorrentSource (..),
+	torrentSource
+) where
+
+import           Control.Monad.Trans.Resource
+
+import           Data.Conduit
+import qualified Data.Conduit.List as C
+import           Data.Conduit.Text
+import           Data.Conduit.Attoparsec
+
+import qualified Data.Text as T
+
+import           Network.HTTP.Client
+
+import           HRel.NodeFilter2
+import           HRel.Network2
+import           HRel.Markup2
+import           HRel.Torrents
+import qualified HRel.XML as X
+
+-- | Pirate Bay source
+pirateBaySource :: NodeFilter [Torrent]
+pirateBaySource =
+	"channel" $/ "item" $//
+		buildTorrent <$> ("title" $/ text)
+		             <*> ("torrent" $/ "magnetURI" $/ text)
+	where
+		buildTorrent title uri =
+			Torrent (T.strip title) (T.strip uri)
+
+-- | RARBG source
+rarbgSource :: NodeFilter [Torrent]
+rarbgSource =
+	"channel" $/ "item" $//
+		buildTorrent <$> ("title" $/ text)
+		             <*> ("link" $/ text)
+	where
+		buildTorrent title uri =
+			Torrent (T.strip title) (T.strip uri)
+
+-- | A source which aggregates torrents
+data TorrentSource
+	= PirateBay String
+	| RARBG String
+	deriving (Show, Eq, Ord)
+
+-- | Process the given torrent source.
+torrentSource :: (MonadResource m) => Manager -> TorrentSource -> Producer m [Torrent]
+torrentSource mgr src =
+	httpRequest mgr url
+		=$= decode utf8
+		=$= processXML
+		=$= filterNodes nf
+	where
+		(url, nf) =
+			case src of
+				PirateBay url -> (url, pirateBaySource)
+				RARBG url     -> (url, rarbgSource)

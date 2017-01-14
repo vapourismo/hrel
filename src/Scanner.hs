99c836f9bdcb9110c615219111812d1d9a703a3d
Ole <ole@vprsm.de>
2017-01-14 20:18:44 +0100

    Migrate 'hrel-scanner' to conduit approach

--- src/Scanner.hs
+++ src/Scanner.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Resource
 
-import           HRel.Sources
-import           HRel.Torrents
-import           HRel.Names
+import           Data.Conduit
+import qualified Data.Conduit.List as C
 
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
@@ -12,35 +13,33 @@ import           Network.HTTP.Client.TLS
 import qualified Database.PostgreSQL.LibPQ as P
 import           Database.PostgreSQL.Store
 
--- |
-torrentSources :: [TorrentSource]
-torrentSources =
-	[RARBG "https://rarbg.to/rssdd_magnet.php?category=41",           -- TV
-	 RARBG "https://rarbg.to/rssdd_magnet.php?category=48;44;45;42"]  -- Movies
+import           HRel.Sources2
+import           HRel.Torrents
+import           HRel.Names
 
--- |
-torrentSourceWorker :: P.Connection -> Manager -> TorrentSource -> IO ()
-torrentSourceWorker db mgr src = do
-	mbTorrents <- processTorrentSource mgr src
-	case mbTorrents of
-		Just torrents ->
-			forM_ torrents $ \ torrent@(Torrent title _) -> do
-				print title
+sources :: (MonadResource m) => Manager -> Source m [Torrent]
+sources mgr =
+	mapM_ (torrentSource mgr)
+	      [RARBG "https://rarbg.to/rssdd_magnet.php?category=41",
+	       RARBG "https://rarbg.to/rssdd_magnet.php?category=48;44;45;42"]
 
-				upsertionResult <- runErrand db $ do
-					tid <- insertTorrent torrent
-					associateTags tid (parseTags title)
+sink :: (MonadResource m) => P.Connection -> Sink [Torrent] m ()
+sink db =
+	C.mapM_ $ \ torrents -> liftIO $
+		forM_ torrents $ \ torrent@(Torrent title _) -> do
+			print title
 
-				case upsertionResult of
-					Left err -> print err
-					_        -> pure ()
+			upsertionResult <- runErrand db $ do
+				tid <- insertTorrent torrent
+				associateTags tid (parseTags title)
 
-		Nothing ->
-			putStrLn ("Source " ++ show src ++ " errored")
+			case upsertionResult of
+				Left err -> print err
+				_        -> pure ()
 
--- |
 main :: IO ()
 main = do
 	db <- P.connectdb "user=hrel dbname=hrel"
 	mgr <- newManager tlsManagerSettings
-	forM_ torrentSources (torrentSourceWorker db mgr)
+	runResourceT $ runConduit $
+		sources mgr =$= sink db
